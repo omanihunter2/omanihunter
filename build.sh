39bf3554ca7b2e4ee8e44fdf2c9c260e3da6d1e1
omanihunter2 <omanihunter@yahoo.com>
2026-06-21 02:02:45 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+export CAMELTOOLS_DATA="$PWD/camel_tools_data"
+
 camel_data -i morphology-db-msa-r13
 camel_data -i disambig-mle-calima-msa-r13
+
+echo "CAMeL data directory:"
+find "$CAMELTOOLS_DATA" -maxdepth 5 -type f | head -50
