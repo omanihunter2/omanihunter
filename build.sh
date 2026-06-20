0753307a03a49dbb96e9f3f51b9abab6b2e57356
omanihunter2 <omanihunter@yahoo.com>
2026-06-21 02:11:54 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-export CAMELTOOLS_DATA="$PWD/camel_tools_data"
+export CAMELTOOLS_DATA="/opt/render/project/src/camel_tools_data"
+mkdir -p "$CAMELTOOLS_DATA"
 
-camel_data -i morphology-db-msa-r13
-camel_data -i disambig-mle-calima-msa-r13
+camel_data -i light
 
-echo "CAMeL data directory:"
-find "$CAMELTOOLS_DATA" -maxdepth 5 -type f | head -50
+find "$CAMELTOOLS_DATA" -maxdepth 6 -type f | grep -E 'morphology.db|mle|calima' || true
